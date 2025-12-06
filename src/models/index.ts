429be8727ec3d15977f86139ab16bcc5cf20286b
simonZt <44356396+simonZt@users.noreply.github.com>
2025-12-06 15:53:04 +0800

    Delete src directory

--- src/models/index.ts
+++
@@ -1,5 +0,0 @@
-// src/models/index.ts
-export * from './Interview';
-export * from './Position';
-export * from './Resume';
-export * from './User';
--- src/types/index.ts
+++
@@ -1,8 +0,0 @@
-// src/types/index.ts
-
-export * from './common.types';
-export * from './interview.types';
-export * from './position.types';
-export * from './resume.types';
-export * from './user.types';
-
